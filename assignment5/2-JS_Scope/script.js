ebf79ccf80c3f5a6d86fdabbdf01a1dc1bc7f5c2
Piyush Kumar <piyusheverhard@gmail.com>
2021-12-15 16:47:13 +0530

    updated files

--- assignment5/2-JS_Scope/script.js
+++ assignment5/2-JS_Scope/script.js
@@ -1,14 +1,19 @@
-let global=69;
-
+let global = 69;
 {
-    let block = 25;
-    console.log(`The block level scope variable is of ${block}`);
-    console.log(`Global level variable can be accessed inside a block. It's value is ${global}`);
-    func();
+  let block = 25;
+  console.log(`The block level scope variable is of ${block}`);
+  console.log(
+    `Global level variable can be accessed inside a block. It's value is ${global}`
+  );
+  func();
 }
 
-function func(){
-    let variable = 98.83;
-    console.log(`Function scope variable can be accessed when the function is called. It's value is ${variable}`);
-    console.log(`Global level variable can be accessed inside a function as well. It's value is ${global}`);
+function func() {
+  let variable = 98.83;
+  console.log(
+    `Function scope variable can be accessed when the function is called. It's value is ${variable}`
+  );
+  console.log(
+    `Global level variable can be accessed inside a function as well. It's value is ${global}`
+  );
 }
